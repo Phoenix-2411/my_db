ff0abfeb331b9dbfe51fa921b688646acf7f9f3f
Phoenix-2411 <nisargphoenix@gmail.com>
2025-02-07 17:31:35 +0530

    Demo-Query-Running

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import db from './db'
+import { initModels } from './models'
+
 import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
-
+initModels(db)
 
 app.use('/api', userRoutes);
 
